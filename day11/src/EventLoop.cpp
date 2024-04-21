dfb00dc4a4bf469aba045f429309cf2cacb57e78
ToniXWD <807077266@qq.com>
2024-04-21 11:21:16 +0800

    day11

---
+++ day11/src/EventLoop.cpp
@@ -0,0 +1,29 @@
+#include "../include/EventLoop.h"
+#include "../include/Channel.h"
+#include "../include/Epoll.h"
+#include "../include/ThreadPool.h"
+#include <vector>
+
+EventLoop::EventLoop() : ep(nullptr), threadPool(nullptr), quit(false) {
+  ep = new Epoll();
+  threadPool = new ThreadPool();
+}
+
+EventLoop::~EventLoop() {
+  delete threadPool;
+  delete ep;
+}
+
+void EventLoop::loop() {
+  while (!quit) {
+    std::vector<Channel *> chs;
+    chs = ep->poll();
+    for (auto it = chs.begin(); it != chs.end(); ++it) {
+      (*it)->handleEvent();
+    }
+  }
+}
+
+void EventLoop::updateChannel(Channel *ch) { ep->updateChannel(ch); }
+
+void EventLoop::addThread(std::function<void()> func) { threadPool->add(func); }
