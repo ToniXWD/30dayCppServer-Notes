d5cf1ce9e951b72e7db0098c9a661ebaa8754087
ToniXWD <807077266@qq.com>
2024-04-22 19:55:33 +0800

    day12

---
+++ day12/src/EventLoop.cpp
@@ -0,0 +1,20 @@
+#include "../include/EventLoop.h"
+#include "../include/Channel.h"
+#include "../include/Epoll.h"
+#include <vector>
+
+EventLoop::EventLoop() : ep(nullptr), quit(false) { ep = new Epoll(); }
+
+EventLoop::~EventLoop() { delete ep; }
+
+void EventLoop::loop() {
+  while (!quit) {
+    std::vector<Channel *> chs;
+    chs = ep->poll();
+    for (auto it = chs.begin(); it != chs.end(); ++it) {
+      (*it)->handleEvent();
+    }
+  }
+}
+
+void EventLoop::updateChannel(Channel *ch) { ep->updateChannel(ch); }
